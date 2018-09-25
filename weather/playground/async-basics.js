ad828ebc9a25bfae87b75ca4b30be57f49729614
Rabel Obispo <rabelobispo@hotmail.com>
2018-09-25 10:07:35 -0400

    Basic async example

---
+++ weather/playground/async-basics.js
@@ -0,0 +1,7 @@
+console.clear();
+console.log("Starting app!");
+
+setTimeout(() => console.log("SetTimeOut"),2000);
+setTimeout(() => console.log("SetTimeOut2"),0);
+
+console.log("Finishing up!");
