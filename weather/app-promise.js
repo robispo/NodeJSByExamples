813b1c3696e308f8b33f61d73908a8455915ee48
Rabel Obispo <rabelobispo@hotmail.com>
2018-09-26 14:54:20 -0400

    request with promise built-in

---
+++ weather/app-promise.js
@@ -0,0 +1,31 @@
+const yargs = require("yargs");
+const axios = require("axios");
+
+var argv = yargs
+  .option({
+    address: {
+      demand: true,
+      alias: "a",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+const encodeAddress = encodeURI(argv.address);
+
+axios
+  .get(
+    `http://www.mapquestapi.com/geocoding/v1/address?key=ZvvIQ83TDDuiml3zAAiw1cpsdfu1pAvy&location=${encodeAddress}`
+  )
+  .then(r => {
+    var lat = r.data.results[0].locations[0].latLng.lat;
+    var lng = r.data.results[0].locations[0].latLng.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/ae83ad34659b2f9368107906bf541724/${lat},${lng}`;
+
+    console.log(r.data.results[0].locations[0].street);
+    return axios.get(weatherUrl);
+  })
+  .then(r => console.log(r.data.currently))
+  .catch(e => console.log("Error"));
