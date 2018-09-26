2b2ed991abfeeef8f2f6803cdda6e01f831afc6a
Rabel Obispo <rabelobispo@hotmail.com>
2018-09-26 17:50:29 -0400

    Using helpers and partials hbs

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -4,15 +4,16 @@ const hbs = require("hbs");
 const app = express();
 const port = 3000;
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
-var getYear = () => new Date().getFullYear();
+hbs.registerHelper("getYear", () => new Date().getFullYear());
+hbs.registerHelper("screamIt", t => t.toUpperCase());
 
 app.get("/", (req, res) => {
   res.render("home", {
     title: "Home page",
-    year: getYear(),
     welcomeMessage:
       "Welcome to a my first page with Node, Express & HBS view engine."
   });
@@ -21,8 +22,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   //   res.send("About!");
   res.render("about", {
-    title: "About page",
-    year: getYear()
+    title: "About page"
   });
 });
 
