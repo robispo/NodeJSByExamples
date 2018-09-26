58809e0366dd1188783678124895f83a3dbc75b4
Rabel Obispo <rabelobispo@hotmail.com>
2018-09-25 23:15:45 -0400

    Advance promise

--- weather/playground/promise.js
+++ weather/playground/promise.js
@@ -1,10 +1,32 @@
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //resolve("Working!");
-    reject("LePuff!!");
-  }, 2500);
-});
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === typeof b) {
+        resolve(a + b);
+      } else {
+        reject("The parameters must be the same type.");
+      }
+    }, 1500);
+  });
+};
 
-console.log("Start!");
+asyncAdd(6, 7)
+  .then(r => {
+    console.log(r);
+    return asyncAdd(r, 4);
+  })
+  .then(r => {
+    console.log(r);
+  })
+  .catch(e => console.log(e));
 
-somePromise.then(message => console.log(message), error => console.log(error));
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     //resolve("Working!");
+//     reject("LePuff!!");
+//   }, 2500);
+// });
+
+// console.log("Start!");
+
+// somePromise.then(message => console.log(message), error => console.log(error));
