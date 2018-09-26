43514dd819c033c4dadb23b8b6d2a741a8a564ab
Rabel Obispo <rabelobispo@hotmail.com>
2018-09-26 16:53:22 -0400

    basic templating in hbs

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,18 +1,29 @@
 const express = require("express");
+const hbs = require("hbs");
+
 const app = express();
 const port = 3000;
 
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
+var getYear = () => new Date().getFullYear();
+
 app.get("/", (req, res) => {
-  res.send({
-    name: "Rabel",
-    likes: ["Hookah", "Drink"]
+  res.render("home", {
+    title: "Home page",
+    year: getYear(),
+    welcomeMessage:
+      "Welcome to a my first page with Node, Express & HBS view engine."
   });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About!");
+  //   res.send("About!");
+  res.render("about", {
+    title: "About page",
+    year: getYear()
+  });
 });
 
 app.get("/bad", (req, res) => {
