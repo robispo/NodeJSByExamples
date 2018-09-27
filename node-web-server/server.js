019ab447868d162fc19c528dc64b0e4a39697470
Rabel Obispo <rabelobispo@hotmail.com>
2018-09-27 16:00:03 -0400

    testing middleware with express

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile("server.log", `${log}\n`, e => {
+    if (e) {
+      console.log(e);
+    }
+  });
+  next();
+});
+// app.use((req, res, next) => res.render("maintenance"));
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getYear", () => new Date().getFullYear());
