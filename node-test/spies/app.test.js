7ae92edbda4a1987ed87ae4d6f5935af00dbc4bb
Rabel Obispo <rabelobispo@hotmail.com>
2018-10-01 15:43:22 -0400

    Using spy to mock functions

--- node-test/spies/app.test.js
+++ node-test/spies/app.test.js
@@ -1,9 +1,27 @@
 const expect = require("expect");
+const rewire = require("rewire");
+
+const app = rewire("./app");
 
 describe("App", () => {
+  const db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__("db", db);
+
   it("should call the spy correctly", () => {
     var spy = expect.createSpy();
     spy();
     expect(spy).toHaveBeenCalled();
+  });
+
+  it("should call db.saveUser", () => {
+    var user = {
+      email: "r@h.com",
+      password: "123"
+    };
+    app.handleSingup(user.email, user.password);
+    expect(db.saveUser).toHaveBeenCalled();
+    expect(db.saveUser).toHaveBeenCalledWith(user);
   });
 });
