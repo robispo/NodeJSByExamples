0929186bd4e013bb754352ff40d163e09b5429da
Rabel Obispo <rabelobispo@hotmail.com>
2018-10-02 16:01:10 -0400

    using mongodb from node

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,49 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017";
+const dbName = "TodoApp";
+
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true },
+  (e, client) => {
+    if (e) {
+      console.log(e);
+      return;
+    }
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    // db.collection("Todos").insertOne(
+    //   {
+    //     text: "Hello world",
+    //     completed: true
+    //   },
+    //   (e, r) => {
+    //     if (e) {
+    //       console.log(e);
+    //       return;
+    //     }
+    //     console.log(JSON.stringify(r.ops, undefined, 2));
+    //   }
+    // );
+
+    db.collection("Users").insertOne(
+        {
+          name: "Rabel",
+          age:29,
+          location:"USA"
+        },
+        (e, r) => {
+          if (e) {
+            console.log(e);
+            return;
+          }
+          console.log(JSON.stringify(r.ops, undefined, 2));
+        }
+      );
+
+    client.close();
+  }
+);
