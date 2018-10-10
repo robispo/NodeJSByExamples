18a5aa8ca0e845a945ad1f1197a64570538075d0
Rabel Obispo <rabelobispo@hotmail.com>
2018-10-10 18:45:46 -0400

    Add test cases for GET /todos/:id

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -1,17 +1,21 @@
 const expect = require("expect");
 const request = require("supertest");
+const { ObjectID } = require("mongodb");
 
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
 
 const todosDumi = [
   {
+    _id: new ObjectID(),
     text: "1"
   },
   {
+    _id: new ObjectID(),
     text: "2"
   },
   {
+    _id: new ObjectID(),
     text: "3"
   }
 ];
@@ -19,8 +23,8 @@ const todosDumi = [
 //Delete all the Todos before run the test.
 beforeEach(done => {
   Todo.deleteMany({})
-  .then(() => Todo.insertMany(todosDumi))
-  .then(() => done());
+    .then(() => Todo.insertMany(todosDumi))
+    .then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -82,7 +86,7 @@ describe("POST /todos", () => {
   });
 });
 
-describe("get /todos", () => {
+describe("GET /todos", () => {
   it("should get all todos", done => {
     request(app)
       .get("/todos")
@@ -90,6 +94,56 @@ describe("get /todos", () => {
       .expect(res => {
         expect(res.body.todos.length).toBeGreaterThan(0);
       })
+      .end((err, res) => {
+        if (err) {
+          done(err);
+          return;
+        }
+
+        done();
+      });
+  });
+});
+
+describe("GET /todos/:id", () => {
+  it("should get a todo by its id", done => {
+    request(app)
+      .get(`/todos/${todosDumi[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo).toExist();
+        expect(res.body.todo.text).toBe(todosDumi[0].text);
+      })
+      .end((err, res) => {
+        if (err) {
+          done(err);
+          return;
+        }
+
+        done();
+      });
+  });
+
+  it("should get a 404 - id not found", done => {
+    var hexId = new ObjectID().toHexString();
+    
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+          return;
+        }
+
+        done();
+      });
+  });
+
+  it("should get a 400 - id not valid", done => {
+    request(app)
+      .get("/todos/123")
+      .expect(400)
       .end((err, res) => {
         if (err) {
           done(err);
